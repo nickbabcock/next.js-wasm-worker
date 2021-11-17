df32ef788125b15dc186976c80c5c2bcfa035014
Nick Babcock <nbabcock19@hotmail.com>
2021-11-16 21:23:24 -0600

    No need to specify webpack5 anymore

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
   webpack(config) {
     const experiments = config.experiments || {};
     config.experiments = {...experiments, asyncWebAssembly: true};
